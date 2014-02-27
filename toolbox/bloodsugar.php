33f3e68ef1c8ea87d9e504a191a2301a058538bc
Anders Akesson <andy_akesson@me.com>
2014-02-27 14:03:38 +0100

    Small changes

---
+++ toolbox/bloodsugar.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Toolbox For Diabetes Aid | Bloodsugar</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700" rel="stylesheet" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/config.js"></script>
+		<script src="js/skel.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+		<!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#bloodsugar">Bloodsugar</a></li>
+					<li><a href="#caregiver">Caregiver</a></li>
+					<li><a href="#history">History</a></li>
+					<li><a href="#calendar">Calendar</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li><a href="#bmi">BMI</a></li>
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<div class="wrapper wrapper-style1">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="8u">
+							<p><label>Fill in blood sugar value here:</label></p>
+							<textarea rows="4" cols="50" placeholder="Blood sugar value" style="font-size:large"></textarea>
+							<p><a href="#work" class="button">Submit</a></p>
+						</div>
+					</div>
+				</article>
+			</div>
+
+	</body>
+</html>
